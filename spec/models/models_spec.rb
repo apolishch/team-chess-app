5eeedeb05e8655178f4aa200e1b0f760b98ce2e1
Stephen Akuffo <slyven@gmail.com>
2016-09-20 03:44:15 +0000

    removed should from factory test. deprecated.

--- spec/models/models_spec.rb
+++ spec/models/models_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Models", type: :model do
   FactoryGirl.factories.map(&:name).each do |factory_name|
     describe "The #{factory_name} factory" do
       it 'is valid' do
-        build(factory_name).should be_valid
+        expect(build(factory_name)).to be_valid
       end
     end
   end

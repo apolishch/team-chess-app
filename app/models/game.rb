3e6db1d2519e13318036bd9a88f48970b8b512eb
Melissa Brown <melissabrown851@gmail.com>
2016-09-28 07:43:56 +0000

    fixed further merge conflicts

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,7 @@
 class Game < ActiveRecord::Base
-<<<<<<< HEAD
 
     belongs_to :user, foreign_key: :player_id
     has_many :pieces
-    
-     
-=======
-  belongs_to :player
-  has_many :pieces
->>>>>>> origin/master
+
+
 end

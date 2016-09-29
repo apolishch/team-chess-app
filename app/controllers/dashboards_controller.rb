cef8bf11ca1b03a8758eeb90b7c6fd6a44538e70
Melissa Brown <melissabrown851@gmail.com>
2016-09-29 01:40:13 +0000

    fixed dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,19 +3,19 @@ class DashboardsController < ApplicationController
     def show
       @games = Game.all
       @current_player_games = current_player_games
-      @opponent_games = opponent_games
+      # @opponent_games = opponent_games
     end
 
     def current_player_games
       current_player = find_current_player
       @current_player_games = current_player.games.limit(10)
+      # Game.where("(white_player_id = #{current_player.id} OR black_player_id = #{current_player.id})
     end
 
-    def opponent_games
-      # is partcipating but as a black player
-      current_player = find_current_player
-      @opponent_games = Game.where("(white_player_id = #{current_player.id} OR black_player_id = #{current_player.id}) AND player_id != #{current_player.id}")
-    end
+
+    #open games TBD
+    #games player white player id or black player id is nil which would be a an open game
+
 
     def find_current_player
       return Player.find(current_user.id)
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
   describe "game#create action" do
-      
+
     it "should successfully create a new game in our database" do
       post :create, game: {
       name: "New_Game",
       }
-      
+
       expect(response).to redirect_to game_path(assigns[:game])
-         
-      gram = Game.last
-      expect(gram.name).to eq("New_Game") 
+
+      game = Game.last
+      expect(game.name).to eq("New_Game")
     end
-    
-    
+
+
      it "should properly deal with validation errors" do
 
       post :create, game: {name: '' }
@@ -22,16 +22,16 @@ RSpec.describe GamesController, type: :controller do
       expect(Game.count).to eq 0
     end
  end
- 
+
  describe "game#show action" do
     it "should successfully show the page if the game is found" do
        game = FactoryGirl.create(:game)
        get :show, id: game.id
        expect(response).to have_http_status(:success)
     end
-    it "should return a 404 error if the game is not found" do 
+    it "should return a 404 error if the game is not found" do
        get :show, id: 'TACOCAT'
        expect(response).to have_http_status(:not_found)
-    end  
-  end   
-end 
+    end
+  end
+end

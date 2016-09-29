7e75bda7087ca5093261f6e5ccf463d624a92336
Melissa Brown <melissabrown851@gmail.com>
2016-09-29 22:35:15 +0000

    playing with tests

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe PlayersController, type: :controller do
 
+  describe "player#create action" do
+
+    it "should successfully create a new player in our database" do
+      game = FactoryGirl.create(:game)
+      player = FactoryGirl.create(:player)
+      expect(response).to redirect_to game_path(assigns[:game])
+
+      player = Player.last
+      expect(player.name).to eq("new_player")
+    end
+
+  end
 end

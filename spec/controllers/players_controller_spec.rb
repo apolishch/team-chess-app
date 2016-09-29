107815f07a7694ee3acc5aa52053ac8d30551189
Stephen Akuffo <slyven@gmail.com>
2016-09-29 22:59:28 +0000

    pending tests

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe PlayersController, type: :controller do
 
     it "should successfully create a new player in our database" do
       player = FactoryGirl.create(:player)
-      player = Player.last
-      expect(player.name).to eq("Foo")
+
+      expect(player.name).to eq(Player.last.name)
     end
 
   end

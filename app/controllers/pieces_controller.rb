0623a4bc049cef112b71ca8ca1e311e75d86277e
Melissa Brown <melissabrown851@gmail.com>
2016-10-01 07:34:45 +0000

    experimenting with adding pieces on chessboard

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,2 +1,19 @@
 class PiecesController < ApplicationController
+
+
+  def show
+    @piece = Piece.new
+  end
+
+  private
+
+  def piece_params
+    @piece_params = params.require(:piece).permit(
+      :x_position,
+      :y_position,
+      :type,
+      :image)
+  end
+
+
 end

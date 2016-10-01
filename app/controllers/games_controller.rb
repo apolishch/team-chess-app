af2e6b85e6243a3f8ec52f3dcf1f0acd1127fa3e
Melissa Brown <melissabrown851@gmail.com>
2016-10-01 07:34:45 +0000

    experimenting with adding pieces on chessboard

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -25,6 +25,18 @@ class GamesController < ApplicationController
     return render_not_found if @game.blank?
   end
 
+  def piece_at(x,y)
+    self.pieces.where(pieces.x_position && pieces.y_position !=nil)
+    # if not equal to nil use html unicode representation
+     if pieces.x_position && pieces.y_position !=nil
+       render @game.piece.image.htmlsafe
+    else
+      return ""
+  end
+
+  # piece_at = pieces.find { |piece| piece.x_position == i && piece.y_position == j }
+  end
+
   private
 
   def game_params

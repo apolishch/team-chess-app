b44fe13ad89a076902709ff23e70f01a10b31865
Melissa Brown <melissabrown851@gmail.com>
2016-10-02 09:17:38 +0000

    added displayed unicode pieces to board

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,21 +11,12 @@ class Game < ActiveRecord::Base
      piece_result = Piece.where(game_id: id, x_position: x, y_position: y)
      if piece_result.empty?
        puts "did not find result "
-       " ------"
+       "---"
      else
-       puts "found results it is #{piece_result[0].image}"
-       piece_result[0].image
+      # puts "found results it is #{piece_result[0].image}"
+       piece_result[0].image.html_safe
      end
   end
-    # self.pieces.find_by(pieces.x_position && pieces.y_position !=nil)
-  # #  if not equal to nil use html unicode representation
-  #   if pieces.x_position && pieces.y_position !=nil
-  #       return piece.image.htmlsafe
-  #   else
-  #     return " "
-  #   end
-
-
 
   def initialize_board
     # White Pieces

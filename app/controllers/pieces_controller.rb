ccb25727e356090681f2438ee128c0cb828f1cd1
Melissa Brown <melissabrown851@gmail.com>
2016-10-13 11:24:09 +0000

    ajax update is working

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,11 +2,13 @@ class PiecesController < ApplicationController
 
   def update
     @piece = Piece.find(params[:id])
-    if @piece.valid_move?(params[:x_position],[:y_position]).update_attributes(x_position: 5, y_position: 1)
-      render json: piece
-    else
-      render text: 'Unauthorized', status: :unauthorized
-    end
+    #valid_move ? x , y
+    # if @piece.valid_move?(params[:x_position],[:y_position])
+       @piece.update_attributes(x_position: params[:x_position], y_position: params[:y_position])
+      # render json: piece
+    # else
+      # render text: 'Unauthorized', status: :unauthorized
+    # end
   end
 end
 

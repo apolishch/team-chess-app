f30e9dd36d80934b3a56c69c429ec95a0890bab7
abraham polishchuk <apolishc@gmail.com>
2016-10-16 18:49:12 +0100

    metaprogramming is eeeeevviiiiiil

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,6 +1,8 @@
 class Piece < ActiveRecord::Base
   belongs_to :game
 
+  DIRECTIONS = %w(diagonal horizontal vertical)
+
   # method to check if the move is inside the board limits
 
   def inside_limits?(x, y)
@@ -52,21 +54,22 @@ class Piece < ActiveRecord::Base
   end
 
   def is_obstructed?(x, y)
-    if vertical?(x, y)
-      vertical_obstructed?(x, y)
-    elsif horizontal?(x, y)
-      horizontal_obstructed?(x, y)
-    elsif diagonal?(x, y)
-      diagonal_obstructed?(x, y)
+    DIRECTIONS.each do |direction|
+      if self.send(:"#{direction}?", x, y)
+        return self.send(:"#{direction}_obstructed?", x, y)
+      end
     end
   end
 
   # method to check if the piece move is vertically obstructed
 
-  def vertical_obstructed?(x, y)
-    (y_min(y)...y_max(y)).each do |y|
-      game.pieces.where('x_position = ? and y_position >= ? and y_position <= ?', x, y_min(y), y_max(y)).each do |piece|
-        if (piece.x_position == x && piece.y_position == y) && piece.id != id
+  def directionally_obstructed(x, y, master_direction)
+    other_direction = (master_direction == 'y') ? 'x' : 'y' 
+    master_min = self.send(:"#{master_direction}_min", eval("#{master_direction}"))
+    master_max = self.send(:"#{master_direction}_max", eval("#{master_direction}"))
+    (master_min..master_max).each do |value|
+      game.pieces.where("#{other_direction}_position = ? and #{master_direction}_position >= ? and #{master_direction}_position <= ?", eval("#{other_direction}"), master_min, master_max).each do |piece|
+        if (piece.send(":#{other_direction}_position") == eval("#{other_direction}" && piece.send(":#{master_diection}") == eval("#{master_direction}")))
           return true
         end
       end
@@ -74,22 +77,31 @@ class Piece < ActiveRecord::Base
     false
   end
 
+  def vertical_obstructed?(x, y)
+    directionally_obstructed(x, y, "y")   
+  end
   # method to check if the piece move is horizontally obstructed
 
   def horizontal_obstructed?(x, y)
-    (x_min(x)...x_max(x)).each do |x|
-      game.pieces.where('y_position = ? and x_position >= ? and x_position <= ?', y, x_min(x), x_max(x)).each do |piece|
-        if (piece.y_position == y && piece.x_position == x) && piece.id != id
-          return true
-        end
-      end
-    end
-    false
+    directionally_obstructed(x, y, "x")
   end
 
   # method to check if the piece move is diagonally obstructed
 
   def diagonal_obstructed?(x, y)
+    l = lambda do |op1, op2|
+      while x_init.send(op1, x) && y_init.send(op2, y)
+        game.pieces.each do |piece|
+          if piece.x_position == x_init && piece.y_position == y_init && piece.id != id
+            return true
+          end
+        end
+        y_init -= 1
+        x_init += 1
+      end
+      false
+    end
+
     x_diff = x - x_position
     y_diff = y - y_position
     x_init = x_position
@@ -108,55 +120,19 @@ class Piece < ActiveRecord::Base
     case direction
       # case when the diag move goes from south to north and to west to east of the board
     when 'SN-WE'
-      while x_init < x && y_init > y
-        game.pieces.each do |piece|
-          if piece.x_position == x_init && piece.y_position == y_init && piece.id != id
-            return true
-          end
-        end
-        y_init -= 1
-        x_init += 1
-        end
-      false
+      return l.call(:<, :>)
 
       # case when the diag move goes from north to south and to west to east of the board
     when 'NS-WE'
-      while x_init < x && y_init < y
-        game.pieces.each do |piece|
-          if piece.x_position == x_init && piece.y_position == y_init && piece.id != id
-            return true
-          end
-        end
-        y_init += 1
-        x_init += 1
-          end
-      false
+      return l.call(:<, :<)
 
       # case when the diag move goes from south to north and from east to west of the board
     when 'SN-EW'
-      while x_init > x && y_init > y
-        game.pieces.each do |piece|
-          if piece.x_position == x_init && piece.y_position == y_init && piece.id != id
-            return true
-          end
-        end
-        y_init -= 1
-        x_init -= 1
-          end
-      false
+      return l.call(:>, :>)
 
       # case when the diag move goes from north to south and from east to west of the board
     when 'NS-EW'
-      while x_init > x && y_init < y
-        game.pieces.each do |piece|
-          if piece.x_position == x_init && piece.y_position == y_init && piece.id != id
-            return true
-          end
-        end
-        y_init += 1
-        x_init -= 1
-          end
-      false
+      return l.call(:>, :<)
 
     end
     end
